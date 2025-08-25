e96ba5fdf2386c38bba6c998e546295512aeb6a2
Ching Jui Young <young.chingjui@gmail.com>
2025-08-25 13:26:40 +0800

    Add Prisma setup with PostgreSQL datasource and initial User model

---
+++ prisma/migrations/20250825052512_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
