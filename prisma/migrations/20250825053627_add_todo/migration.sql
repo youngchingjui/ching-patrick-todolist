15e2ea2a17ce38c97127b45d59b04760129a318d
Ching Jui Young <young.chingjui@gmail.com>
2025-08-25 13:38:26 +0800

    Add Todo model and migration, update Prisma schema with direct URL for database

---
+++ prisma/migrations/20250825053627_add_todo/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "public"."Todo" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
